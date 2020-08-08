3ac00c7135e115a2c973bd7d5572aedcb10f1274
github username <github email address>
2020-08-08 17:15:43 -0400

    done

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+
+
+Project.destroy_all
+
+#projects --------------------
+project1 = Project.create(name: 'Boo!')
+project2 = Project.create(name: 'Boom Boom')
+project3 = Project.create(name: 'Blargggh')
